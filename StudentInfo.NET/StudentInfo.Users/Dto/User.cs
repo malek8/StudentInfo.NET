354c1bdb2700ba2c60e297370617f72de50349a5
Malek Atwiz <matwiz@futurecode.ca>
2017-09-14 23:04:24 -0400

    Replacing Web project

---
+++ StudentInfo.NET/StudentInfo.Users/Dto/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo.Users.Dto
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
