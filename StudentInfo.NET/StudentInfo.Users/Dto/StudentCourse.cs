b2c121b4f0cc6fdefa0e778938223f426cc49eaa
Malek Atwiz <matwiz@futurecode.ca>
2017-11-15 22:29:09 -0500

    My Courses.

--- StudentInfo.NET/StudentInfo.Users/Dto/StudentCourse.cs
+++ StudentInfo.NET/StudentInfo.Users/Dto/StudentCourse.cs
@@ -7,13 +7,14 @@ using StudentInfo.Enums;
 
 namespace StudentInfo.Users.Dto
 {
-  public class StudentCourse
-  {
-    public Guid Id { get; set; }
-    public Guid StudentId { get; set; }
-    public SemesterCourse CourseSemester { get; set; }
-    public DateTime CreateDate { get; set; }
-    public DateTime LastUpdate { get; set; }
-    public CourseRegistrationState CourseState { get; set; }
-  }
+    public class StudentCourse
+    {
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public string Grade { get; set; }
+        public virtual SemesterCourse CourseSemester { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public CourseRegistrationState CourseState { get; set; }
+    }
 }
