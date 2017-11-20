321d6c1d571ed753638bba70e0573ad00c3c18c2
Malek Atwiz <Malek.Atwiz@MAPLESANDCALDER.com>
2017-11-20 16:36:26 -0500

    Assign instructor

--- StudentInfo.NET/StudentInfo.WebClient/Scripts/studentinfojs.js
+++ StudentInfo.NET/StudentInfo.WebClient/Scripts/studentinfojs.js
@@ -1,22 +1,22 @@
 ﻿function loadCourseDetails(courseId, allowEdit) {
-    $.get('/Course/Details?id=' + courseId + '&allowEdit=' + allowEdit, function (data) {
-        $('#courseDetailsModalBody').html(data);
-        $('#courseDetailsModal').modal('show');
-    })
+    $.get("/Course/Details?id=" + courseId + "&allowEdit=" + allowEdit, function (data) {
+        $("#courseDetailsModalBody").html(data);
+        $("#courseDetailsModal").modal("show");
+    });
 }
 
 function loadSemesterCourseDetails(courseId) {
-    $.get('/Course/SemesterCourseDetails?id=' + courseId, function (data) {
-        $('#courseDetailsModalBody').html(data);
-        $('#courseDetailsModal').modal('show');
-    })
+    $.get("/Course/SemesterCourseDetails?id=" + courseId, function (data) {
+        $("#courseDetailsModalBody").html(data);
+        $("#courseDetailsModal").modal("show");
+    });
 }
 
 function loadTeachersModal(semesterCourseId) {
-    $.get('/Course/GetInstructor?semesterCourseId=' + semesterCourseId, function (data) {
-        $('#courseInstructorModalBody').html(data);
-        $('#courseInstructorModal').modal('show');
-    })
+    $.get("/Course/GetInstructor?semesterCourseId=" + semesterCourseId, function (data) {
+        $("#courseInstructorModalBody").html(data);
+        $("#courseInstructorModal").modal("show");
+    });
 }
 
 function showValidationIssues(isShown) {
@@ -31,10 +31,10 @@ function showValidationIssues(isShown) {
 function fillValidationErrors(errors) {
     $("#errors").html("");
 
-    var errorsList = document.createElement('ul');
+    var errorsList = document.createElement("ul");
 
     for (var i = 0; i < errors.length; i++) {
-        var item = document.createElement('li');
+        var item = document.createElement("li");
         item.appendChild(document.createTextNode(errors[i]));
         errorsList.appendChild(item);
     }
@@ -45,7 +45,7 @@ function fillValidationErrors(errors) {
 function processResult(data) {
     if (data.success) {
         showValidationIssues(false);
-        $('#courseDetailsModal').modal('hide');
+        $("#courseDetailsModal").modal("hide");
     }
     else {
         fillValidationErrors(data);
@@ -55,18 +55,18 @@ function processResult(data) {
 
 function updateDepartments() {
     $("#departmentSelector").empty();
-    getDepartments($('#facultySelector').val());
+    getDepartments($("#facultySelector").val());
 }
 
 function getDepartments(facultyId) {
-    var url = '/Course/GetDepartments';
+    var url = "/Course/GetDepartments";
     $.ajax({
-        type: 'GET',
+        type: "GET",
         url: url,
         data: { facultyId },
         success: function (data) {
             for (var i = 0; i < data.length; i++) {
-                $('#departmentSelector').append('<option value = "' + data[i].value + '">' + data[i].text + '</option>');
+                $("#departmentSelector").append('<option value = "' + data[i].value + '">' + data[i].text + '</option>');
             }
         }
     });
@@ -74,27 +74,27 @@ function getDepartments(facultyId) {
 
 function enrollCourse(semesterCourseId) {
     $.ajax({
-        type: 'POST',
-        url: '/Course/Enroll',
+        type: "POST",
+        url: "/Course/Enroll",
         data: { semesterCourseId },
         success: function (data) {
             displayAlert(data.message, data.success);
         }
-    })
+    });
 }
 
 function dropCourse(studentCourseId) {
     $.ajax({
-        type: 'POST',
-        url: '/Course/Drop',
+        type: "POST",
+        url: "/Course/Drop",
         data: { studentCourseId },
         success: function (data) {
             if (data.success === true) {
-                $('#tr_item' + studentCourseId).remove();
+                $("#tr_item" + studentCourseId).remove();
             }
             displayAlert(data.message, data.success);
         }
-    })
+    });
 }
 
 function displayAlert(messageContent, state) {
@@ -118,13 +118,14 @@ function displayAlert(messageContent, state) {
     $("#" + alertDivId).delay(5000).fadeOut();
 }
 
-function assignInstructor(userId, semesterCourseId) {
+function assignInstructor(semesterCourseId) {
+    var userId = $("#instructorsDropdown").val();
     $.ajax({
-        type: 'POST',
-        url: '/Course/AssignInstructor',
+        type: "POST",
+        url: "/Course/AssignInstructor",
         data: { userId, semesterCourseId },
         success: function (data) {
             displayAlert(data.message, data.success);
         }
-    })
+    });
 }
