d7eb6dcc111b41f3389badf1c8d7c7bae15776be
Malek Atwiz <malekatwiz@gmail.com>
2017-12-27 19:53:27 -0500

    student search

---
+++ StudentInfo.NET/StudentInfo.WebClient/Controllers/ManageStudentsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentInfo.Enums;
+using StudentInfo.Dto;
+using StudentInfo.WebClient.Helpers;
+using StudentInfo.WebClient.Models;
+using StudentInfo.Data;
+
+namespace StudentInfo.WebClient.Controllers
+{
+    public class ManageStudentsController : Controller
+    {
+        [Authorize]
+        [AuthorizeRoles(SystemRoles.Administrator, SystemRoles.FacultyMember, SystemRoles.Advisor)]
+        public ActionResult Search(StudentSearchModel model, int? page)
+        {
+            if (model == null) model = new StudentSearchModel();
+
+            var db = new StudentInfoContext();
+
+            var students = db.Students.ToList();
+
+            if (!string.IsNullOrEmpty(model.Name))
+            {
+                
+            }
+
+            return View();
+        }
+    }
+}
---
+++ StudentInfo.NET/StudentInfo.WebClient/Models/StudentSearchModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentInfo.WebClient.Models
+{
+    public class StudentSearchModel
+    {
+        [Display(Name = "Student ID")]
+        public string StudentId { get; set; }
+
+        [Display(Name = "Department")]
+        public Guid DepartmentId { get; set; }
+
+        [Display(Name = "Student Name")]
+        public string Name { get; set; }
+    }
+}
